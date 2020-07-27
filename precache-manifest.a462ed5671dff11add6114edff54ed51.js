539e081d3c7072d1686dedda38d85c1086fb1cf3
sarafotros <sarafotros@gmail.com>
2020-07-27 10:45:38 +0100

    Updates

---
+++ precache-manifest.a462ed5671dff11add6114edff54ed51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d55b579f5ce7e4fb0ef8dd485c87102",
+    "url": "/sarafotros/calculator-react-2/index.html"
+  },
+  {
+    "revision": "3ef0587ba1903df0da77",
+    "url": "/sarafotros/calculator-react-2/static/css/main.7fa15487.chunk.css"
+  },
+  {
+    "revision": "a1e165aa7b35c4961952",
+    "url": "/sarafotros/calculator-react-2/static/js/2.2179ff43.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/sarafotros/calculator-react-2/static/js/2.2179ff43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef0587ba1903df0da77",
+    "url": "/sarafotros/calculator-react-2/static/js/main.f8b34c7c.chunk.js"
+  },
+  {
+    "revision": "b80c209e01f73f16109c",
+    "url": "/sarafotros/calculator-react-2/static/js/runtime-main.a2194a2c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sarafotros/calculator-react-2/precache-manifest.a462ed5671dff11add6114edff54ed51.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sarafotros/calculator-react-2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
